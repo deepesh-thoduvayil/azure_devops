ad301e1a906cd0d16d5a4cb936b5354c91d68e45
deepesh-thoduvayil <49249547+deepesh-thoduvayil@users.noreply.github.com>
2022-07-06 11:50:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,6 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   	bucket = "test-s3-bucket-300575"
   	acl = "private"
-  	versioning {
-    		enabled = true
-  	}
 
 	tags = {
     		Environment = "test"
