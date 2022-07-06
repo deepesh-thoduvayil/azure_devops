3fda20c2cc1fe2a42d0190b078802c74120147cd
deepesh-thoduvayil <49249547+deepesh-thoduvayil@users.noreply.github.com>
2022-07-06 11:58:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-provider "aws" {
-  	region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_s3_bucket" "example" {
-  	bucket = "test-s3-bucket-300575"
-  	acl = "private"
+provider "aws" {
+  region  = "us-west-2"
+}
 
-	tags = {
-    		Environment = "test"
-  	}
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
