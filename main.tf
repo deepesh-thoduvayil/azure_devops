dac75af39929936a1a06df7890fdd950b878cbf7
deepesh-thoduvayil <49249547+deepesh-thoduvayil@users.noreply.github.com>
2022-07-06 11:06:02 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  	region = "eu-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  	bucket = "test-s3-bucket-300575"
+  	acl = "private"
+  	versioning {
+    		enabled = true
+  	}
+
+	tags = {
+    		Environment = "test"
+  	}
+}
